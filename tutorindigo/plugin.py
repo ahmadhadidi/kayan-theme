40d1bd21a35d45fce4cf60751efd820c3f50053e
Ahmad Hadidi <ahmadhadidi@gmail.com>
2023-06-14 13:08:02 +0300

    Update plugin.py
    Add kayan's stuff here.

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -4,14 +4,15 @@ from tutor import hooks
 
 from .__about__ import __version__
 
+# Ref: https://discuss.openedx.org/t/tutor-indigo-theme-with-new-plugin/7673/11
 
 ################# Configuration
 config = {
     # Add here your new settings
     "defaults": {
         "VERSION": __version__,
-        "WELCOME_MESSAGE": "The place for all your online learning",
-        "PRIMARY_COLOR": "#3b85ff",  # cool blue
+        "WELCOME_MESSAGE": "Welcome to Kayan's Academy",
+        "PRIMARY_COLOR": "#1968b0",  # Kayan Blue
         # Footer links are dictionaries with a "title" and "url"
         # To remove all links, run:
         # tutor config save --set INDIGO_FOOTER_NAV_LINKS=[] --set INDIGO_FOOTER_LEGAL_LINKS=[]
@@ -22,8 +23,8 @@ config = {
         "FOOTER_LEGAL_LINKS": [
             {"title": "Terms of service", "url": "/tos"},
             {
-                "title": "Indigo theme for Open edX",
-                "url": "https://github.com/overhangio/tutor-indigo",
+                "title": "Kayan Academy",
+                "url": "https://kayanhr.com",
             },
         ],
     },
@@ -38,20 +39,20 @@ hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
 # This is where the theme is rendered in the openedx build directory
 hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
     [
-        ("indigo", "build/openedx/themes"),
+        ("kayanhr", "build/openedx/themes"),
     ],
 )
 
 # Force the rendering of scss files, even though they are included in a "partials" directory
 hooks.Filters.ENV_PATTERNS_INCLUDE.add_item(
-    r"indigo/lms/static/sass/partials/lms/theme/"
+    r"kayanhr/lms/static/sass/partials/lms/theme/"
 )
 
 # Load all configuration entries
 hooks.Filters.CONFIG_DEFAULTS.add_items(
-    [(f"INDIGO_{key}", value) for key, value in config["defaults"].items()]
+    [(f"KAYAN_{key}", value) for key, value in config["defaults"].items()]
 )
 hooks.Filters.CONFIG_UNIQUE.add_items(
-    [(f"INDIGO_{key}", value) for key, value in config["unique"].items()]
+    [(f"KAYAN_{key}", value) for key, value in config["unique"].items()]
 )
 hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
